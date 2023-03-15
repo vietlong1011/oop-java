db9068a63f2fa207c626ce1b99698b13f95c1101
vietlong1011 <longlieuliny@gmail.com>
2023-03-16 01:20:44 +0700

    update  massage delete

--- src/main/java/exercise_nine/job/Functions.java
+++ src/main/java/exercise_nine/job/Functions.java
@@ -22,7 +22,7 @@ public class Functions {
         int homeNumber = Integer.parseInt(sc.nextLine());
         System.out.print("Nhap ma so cong to dien: ");
         int electricNumber = Integer.parseInt(sc.nextLine());
-        Person person = new Person(namePerson,homeNumber,electricNumber);
+        Person person = new Person(namePerson, homeNumber, electricNumber);
         return person;
     }
 
@@ -36,34 +36,38 @@ public class Functions {
             while (checkBill) {
                 Person person = addPerson();
                 System.out.print("Nhap chi so dien cu: ");
-               int electricOld = Integer.parseInt(sc.nextLine());
+                int electricOld = Integer.parseInt(sc.nextLine());
                 System.out.print("Nhap chi so dien moi: ");
                 int electricNew = Integer.parseInt(sc.nextLine());
                 int prince = 1;
                 if (electricNew > electricOld) {
-                   prince = ((electricNew - electricOld) * 5);
+                    prince = ((electricNew - electricOld) * 5);
                     checkBill = false;
                 } else {
                     System.out.println("Gia tri khong hop le ,nhap lai");
                 }
-                Bill bill = new Bill(electricOld,electricNew,prince,person);
+                Bill bill = new Bill(electricOld, electricNew, prince, person);
                 this.billList.add(bill);
             }
         }
 
     }
+
     public void deleteBillPerson() {
         System.out.print("Nhap so nha can xoa : ");
         int delete = Integer.parseInt(this.sc.nextLine());
-            for (Bill b : billList) {
-               // if (String.valueOf(delete).equals(String.valueOf(b.getPerson().getHomeNumber()))) {
-                if (delete == b.getPerson().getHomeNumber()) {
-                    this.billList.remove(b);
-                    System.out.println("Delete successfully " );
-                }else {
-                    System.out.println("Xoa that bai");
-                }
+        boolean checkHome = true;
+        for (Bill b : billList) {
+            // if (String.valueOf(delete).equals(String.valueOf(b.getPerson().getHomeNumber()))) {
+            if (delete == b.getPerson().getHomeNumber()) {
+                this.billList.remove(b);
+                System.out.println("Delete successfully ");
+                checkHome = false;
             }
+        }
+        if (checkHome) {
+            System.out.println("Xoa that bai");
+        }
     }
 
     public void display() {
